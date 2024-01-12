751ffad402c5873a260a1c7d7919d2b0af03d7c9
Igwanya <felixmuthui32@gmail.com>
2024-01-12 14:05:36 +0300

    Modified tailwind loader options from webpack

--- public/postcss.config.js
+++ public/postcss.config.js
@@ -1,10 +1,17 @@
 /** @type {import('postcss-load-config').Config} */
 const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
     autoprefixer: {},
-
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   }
 }
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'assets/js'),
+        path: path.resolve(__dirname, 'assets/dist'),
     },
     module: {
       rules: [
@@ -15,7 +15,29 @@ module.exports = {
          test: /\.css$/i,
          exclude: /node_modules/,
          include: path.resolve(__dirname, 'assets/src'),
-         use: ['style-loader', 'css-loader', 'postcss-loader'],
+         use: [
+            'style-loader',
+            'css-loader',
+             {
+                 loader: 'postcss-loader',
+                 options: {
+                     postcssOptions: {
+                         plugins: [
+                             [
+                                 'postcss-preset-env'
+                             ],
+
+                         ],
+                     },
+                 },
+             },
+         ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'assets/resource',
+          include: path.resolve(__dirname, 'assets/src'),
+          loader: 'url-loader',
         },
       ],
     },
