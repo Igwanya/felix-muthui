228a6bd9d3d679e6569d5f0d034dbbe3c9f042c0
Igwanya <felixmuthui32@gmail.com>
2024-01-12 17:24:47 +0300

    Modified tailwind src configs and removed npm concurently -k arg

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
-    './assets/src/**/*.{html,js}'
+  content: {
+      relative: true,
+      files: [
+        './_drafts/**/*.html',
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './_posts/*.md',
+        './*.md',
+        './*.html',
+        './assets/src/**/*.{html,js}',
+        './pages/**/*.{html,js}',
+        './components/**/*.{html,js}',
+        './**/*.{html,js}',
   ],
   theme: {
     extend: {},
   },
   plugins: []
 }
+};
