355e3736f1c78846a9687f6dd1099c018793cb5a
Igwanya <felixmuthui32@gmail.com>
2024-01-14 12:52:22 +0300

    Updated the base html to use global variables

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ echo "[*] swicthing to public directory"
 cd public
 npm run build
 cd ..
-echo " " && fortune -i echo " "
+echo " " && fortune echo " "
 # npx tailwindcss -i /home/felixmuthui32/projects/felix-muthui/public/assets/tailwindcss/input.css -o /home/felixmuthui32/projects/felix-muthui/public/assets/css/output.css
 
 bundle exec jekyll serve --livereload
